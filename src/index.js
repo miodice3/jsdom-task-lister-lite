96fb864ad2193139799e93bc6f1f5ef9b746f34c
Michael Iodice <miodice3@gmail.com>
2021-01-07 19:06:25 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+
+let todoForm = document.getElementById('create-task-form')
+let todoInput = document.querySelector("#new-task-description")
+let todoList = document.querySelector("#tasks")
+
+todoForm.addEventListener('submit', handleSubmit)
+todoList.addEventListener('click', handleClickTodo)
+
+function handleSubmit(e){
+  e.preventDefault()
+  // alert('The form was submitted!')
+  // console.log("the event e is:", e)
+  let todo = todoInput.value
+  let todoHTML = `<li>${todo}</li>`
+  // let todoHTML = `<li><button value='${todo}' >X</button></li>`
+  todoList.innerHTML += todoHTML
+  todoInput.value = ""
+}
+
+function handleClickTodo(e){
+  console.log("one of the LIs was clicked, it was:", e.target)
+  e.target.remove()
+}
+
